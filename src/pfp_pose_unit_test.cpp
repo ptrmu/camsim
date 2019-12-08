4e064ea78a47c4b7b52a389a492f32b6480401e9
Peter Mullen <ptrmu@outlook.com>
2019-12-08 12:30:21 -0800

    start test

--- src/pfp_pose_unit_test.cpp
+++ src/pfp_pose_unit_test.cpp
@@ -1,10 +1,18 @@
 
 #include "pfp_run.hpp"
 
+#include "gtsam/geometry/Pose2.h"
+#include "gtsam/geometry/Pose3.h"
+
+using namespace gtsam;
+
 namespace camsim
 {
   void pfp_pose_unit_test()
   {
+    Pose3 base_pose3{};
 
+    auto adjoint = base_pose3.AdjointMap();
+    
   }
 }
