b8f1b47470a2368e2b19333d5475ecbdbbd8f686
Peter Mullen <ptrmu@outlook.com>
2020-01-26 21:02:53 -0800

    test camera markers

--- src/pfp_gspnp.cpp
+++ src/pfp_gspnp.cpp
@@ -1,11 +1,43 @@
 
 #include "pfp_run.hpp"
 
+#include "model.hpp"
+
 namespace camsim
 {
+  typedef std::reference_wrapper<const MarkerModel> MarkerModelRef;
+
+  static void test_one_camera(const CameraModel &camera,
+                              const std::vector<MarkerModelRef> &marker_refs)
+  {
+  }
 
   void pfp_gspnp()
   {
+    int n_markers = 8;
+    int n_cameras = 32;
+
+    Model model{ModelConfig{PoseGens::CircleInXYPlaneFacingAlongZ{n_markers, 2., 2., false},
+                            PoseGens::CircleInXYPlaneFacingAlongZ{n_cameras, 2., 0., true},
+                            camsim::CameraTypes::simulation,
+                            0.1775}};
+
+    // Loop over all the cameras
+    for (auto &camera : model.cameras_.cameras_) {
+      std::vector<MarkerModelRef> marker_refs{};
+
+      // Figure out which markers are visible from this camera
+      for (auto &marker : model.markers_.markers_) {
+        if (!model.corners_f_images_[camera.index()][marker.index()].corners_f_image_.empty()) {
+          marker_refs.emplace_back(MarkerModelRef{marker});
+        }
+      }
+
+      // Let the solver work on these measurements.
+      if (marker_refs.size() > 1) {
+        test_one_camera(camera, marker_refs);
+      }
+    }
 
   }
 }
