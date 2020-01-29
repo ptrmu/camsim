c212b2de30a8eb3456a1718ee0b5a88648510fb5
Peter Mullen <ptrmu@outlook.com>
2020-01-28 21:40:52 -0800

    camera_f_marker

--- src/pfp_gspnp.cpp
+++ src/pfp_gspnp.cpp
@@ -2,6 +2,7 @@
 #include "pfp_run.hpp"
 
 #include "model.hpp"
+#include "pose_with_covariance.hpp"
 
 namespace camsim
 {
@@ -11,6 +12,14 @@ namespace camsim
   {
     Model &model_;
 
+    void calc_gspnp(const CameraModel &camera,
+                    const MarkerModel &marker,
+                    const std::vector<gtsam::Point2> corners_f_image)
+    {
+      auto camera_f_marker = camera.camera_f_world_.inverse() * marker.marker_f_world_;
+      std::cout << PoseWithCovariance::to_str(camera_f_marker) << std::endl;
+    }
+
   public:
     TestGspnp(Model &model) :
       model_{model}
@@ -19,9 +28,10 @@ namespace camsim
     void operator()(const CameraModel &camera,
                     const std::vector<MarkerModelRef> &marker_refs)
     {
-      for (auto &marker : marker_refs) {
-
-        
+      for (auto &marker_ref : marker_refs) {
+        auto &marker = marker_ref.get();
+        auto &corners_f_image = model_.corners_f_images_[camera.index()][marker.index()];
+        calc_gspnp(camera, marker, corners_f_image.corners_f_image_);
       }
     }
   };
@@ -34,12 +44,17 @@ namespace camsim
   void pfp_gspnp()
   {
     int n_markers = 8;
-    int n_cameras = 32;
+    int n_cameras = 8;
 
-    Model model{ModelConfig{PoseGens::CircleInXYPlaneFacingAlongZ{n_markers, 2., 2., false},
-                            PoseGens::CircleInXYPlaneFacingAlongZ{n_cameras, 2., 0., true},
+    Model model{ModelConfig{PoseGens::CircleInXYPlaneFacingOrigin{n_markers, 2.},
+                            PoseGens::SpinAboutZAtOriginFacingOut{n_cameras},
                             camsim::CameraTypes::simulation,
                             0.1775}};
+
+//    Model model{ModelConfig{PoseGens::CircleInXYPlaneFacingAlongZ{n_markers, 2., 2., false},
+//                            PoseGens::CircleInXYPlaneFacingAlongZ{n_cameras, 2., 0., true},
+//                            camsim::CameraTypes::simulation,
+//                            0.1775}};
 
     TestGspnp test_gspnp{model};
 
