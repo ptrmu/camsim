f011c5827c36eacd344848f1beef931fdc456872
Peter Mullen <ptrmu@outlook.com>
2020-05-07 21:55:59 -0700

    FilterState

--- src/kal_kalman_filter_x.cpp
+++ src/kal_kalman_filter_x.cpp
@@ -8,6 +8,15 @@
 
 namespace camsim
 {
+
+// ==============================================================================
+// KalmanFilterX class
+// ==============================================================================
+
+  // A class the implements a kalman filter. This filter is implemented using
+  // opencv's Matx matricies. The implementation followes the opencv KalmanFilter
+  // implementation.
+
   template<typename Type, int StateDims, int MeasurementDims>
   class KalmanFilterX
   {
@@ -17,82 +26,65 @@ namespace camsim
     using StateCovType = cv::Matx<Type, StateDims, StateDims>;
     using MeasurementCovType = cv::Matx<Type, MeasurementDims, MeasurementDims>;
 
-    cv::Matx<Type, StateDims, StateDims> transition_matrix;        // state transition matrix (A)
-    cv::Matx<Type, MeasurementDims, StateDims> measurement_matrix; // measurement matrix (H)
+  private:
+    // temporary matrices
+    cv::Matx<Type, StateDims, StateDims> temp1;
+    cv::Matx<Type, MeasurementDims, StateDims> temp2;
+    cv::Matx<Type, MeasurementDims, MeasurementDims> temp3;
+    cv::Matx<Type, MeasurementDims, StateDims> temp4;
+    cv::Vec<Type, MeasurementDims> temp5;
+
+    StateType x_k_prime;                              // predicted state (x'(k)): x(k)=A*x(k-1)+B*u(k)
+    StateCovType error_cov_k_prime;                   // priori error estimate covariance matrix (P'(k)): P'(k)=A*P(k-1)*At + Q)*/
+    cv::Matx<Type, StateDims, MeasurementDims> gain;  // Kalman gain matrix (K(k)): K(k)=P'(k)*Ht*inv(H*P'(k)*Ht+R)
+
+  public:
+    cv::Matx<Type, StateDims, StateDims> transition_matrix;         // state transition matrix (A)
+    cv::Matx<Type, MeasurementDims, StateDims> measurement_matrix;  // measurement matrix (H)
     StateCovType process_noise_cov;                                 // process noise covariance matrix (Q)
     MeasurementCovType measurement_noise_cov;                       // measurement noise covariance matrix (R)
 
-    struct Separate
+    struct FilterState
     {
-      KalmanFilterX &kf_;
-
-      StateType statePre;                               // predicted state (x'(k)): x(k)=A*x(k-1)+B*u(k)
-      StateType state_post;                              // corrected state (x(k)): x(k)=x'(k)+K(k)*(z(k)-H*x'(k))
-      StateCovType errorCovPre;                         // priori error estimate covariance matrix (P'(k)): P'(k)=A*P(k-1)*At + Q)*/
-      StateCovType error_cov_post;                        // posteriori error estimate covariance matrix (P(k)): P(k)=(I-K(k)*H)*P'(k)
-      cv::Matx<Type, StateDims, MeasurementDims> gain;  // Kalman gain matrix (K(k)): K(k)=P'(k)*Ht*inv(H*P'(k)*Ht+R)
-
-      // temporary matrices
-      cv::Matx<Type, StateDims, StateDims> temp1;
-      cv::Matx<Type, MeasurementDims, StateDims> temp2;
-      cv::Matx<Type, MeasurementDims, MeasurementDims> temp3;
-      cv::Matx<Type, MeasurementDims, StateDims> temp4;
-      cv::Vec<Type, MeasurementDims> temp5;
-
-      explicit Separate(KalmanFilterX &kf); //
-      const StateType &predict(); //
-      const StateType &update(const MeasurementType &measurement); //
+      StateType x_k;                                  // corrected state (x(k)): x(k)=x'(k)+K(k)*(z(k)-H*x'(k))
+      StateCovType error_cov_k;                       // posteriori error estimate covariance matrix (P(k)): P(k)=(I-K(k)*H)*P'(k)
     };
 
+    const StateType &predict(FilterState &fs); //
+    const StateType &update(FilterState &fs,
+                            const MeasurementType &measurement); //
   };
 
   template<typename Type, int StateDims, int MeasureDims>
-  KalmanFilterX<Type, StateDims, MeasureDims>::Separate::Separate(KalmanFilterX &kf) :
-    kf_{kf}
-  {}
-
-//  {
-//    transition_matrix = {1, 1, 0, dt};
-//    measurement_matrix = {1, 0};
-//    process_noise_cov = StateCovType{dt * dt, dt, dt, 1.} * process_std_dev * process_std_dev;
-//    measurement_noise_cov = MeasurementCovType{measurement_std_dev * measurement_std_dev};
-//  }
-
-//  template<typename Type, int StateDims, int MeasureDims>
-//  void KalmanFilterX<Type, StateDims, MeasureDims>::init()
-//  {
-//  }
-
-  template<typename Type, int StateDims, int MeasureDims>
   const typename KalmanFilterX<Type, StateDims, MeasureDims>::StateType &
-  KalmanFilterX<Type, StateDims, MeasureDims>::Separate::predict()
+  KalmanFilterX<Type, StateDims, MeasureDims>::predict(FilterState &fs)
   {
     // update the state: x'(k) = A*x(k)
-    statePre = kf_.transition_matrix * state_post;
+    x_k_prime = transition_matrix * fs.x_k;
 
     // update error covariance matrices: temp1 = A*P(k)
-    temp1 = kf_.transition_matrix * error_cov_post;
+    temp1 = transition_matrix * fs.error_cov_k;
 
     // P'(k) = temp1*At + Q
-    cv::gemm(temp1, kf_.transition_matrix, 1, kf_.process_noise_cov, 1, errorCovPre, cv::GEMM_2_T);
+    cv::gemm(temp1, transition_matrix, 1, process_noise_cov, 1, error_cov_k_prime, cv::GEMM_2_T);
 
     // handle the case when there will be measurement before the next predict.
-    state_post = statePre;
-    error_cov_post = errorCovPre;
+    fs.x_k = x_k_prime;
+    fs.error_cov_k = error_cov_k_prime;
 
-    return statePre;
+    return x_k_prime;
   }
 
   template<typename Type, int StateDims, int MeasureDims>
   const typename KalmanFilterX<Type, StateDims, MeasureDims>::StateType &
-  KalmanFilterX<Type, StateDims, MeasureDims>::Separate::update(
-    const cv::Vec<Type, MeasureDims> &measurement)
+  KalmanFilterX<Type, StateDims, MeasureDims>::update(FilterState &d,
+                                                      const MeasurementType &measurement)
   {
     // temp2 = H*P'(k)
-    temp2 = kf_.measurement_matrix * errorCovPre;
+    temp2 = measurement_matrix * error_cov_k_prime;
 
     // temp3 = temp2*Ht + R
-    cv::gemm(temp2, kf_.measurement_matrix, 1, kf_.measurement_noise_cov, 1, temp3, cv::GEMM_2_T);
+    cv::gemm(temp2, measurement_matrix, 1, measurement_noise_cov, 1, temp3, cv::GEMM_2_T);
 
     // temp4 = inv(temp3)*temp2 = Kt(k)
     solve(temp3, temp2, temp4, cv::DECOMP_SVD);
@@ -101,16 +93,20 @@ namespace camsim
     gain = temp4.t();
 
     // temp5 = z(k) - H*x'(k)
-    temp5 = measurement - kf_.measurement_matrix * statePre;
+    temp5 = measurement - measurement_matrix * x_k_prime;
 
     // x(k) = x'(k) + K(k)*temp5
-    state_post = statePre + gain * temp5;
+    d.x_k = x_k_prime + gain * temp5;
 
     // P(k) = P'(k) - K(k)*temp2
-    error_cov_post = errorCovPre - gain * temp2;
+    d.error_cov_k = error_cov_k_prime - gain * temp2;
 
-    return state_post;
+    return d.x_k;
   }
+
+// ==============================================================================
+// Visualizer class
+// ==============================================================================
 
   template<int Cols, int Rows>
   class Visualizer
@@ -183,40 +179,44 @@ namespace camsim
     imshow("Kalman", img_);
   }
 
+// ==============================================================================
+// CVFType class
+// ==============================================================================
+
+// Constant Velocity Kalman Filter. This class has a 2 element state vector. The
+// state variables are position and velocity.
 
   class CVFType : public KalmanFilterX<float, 2, 1>
   {
-    float process_std_dev_;
-    float measurement_std_dev_;
-
   public:
-    CVFType(float process_std_dev,
-            float measurement_std_dev); //
-    void init(float dt); //
-    void init_separate(float measurement_0,
-                       Separate &separate); //
+    CVFType() = default; //
+    void init(float dt,
+              float process_std_dev,
+              float measurement_std_dev); //
+    void init_filter_state(float measurement_0,
+                           FilterState &fs); //
   };
 
-  CVFType::CVFType(float process_std_dev,
-                   float measurement_std_dev) :
-    process_std_dev_{process_std_dev},
-    measurement_std_dev_{measurement_std_dev}
-  {}
-
-  void CVFType::init(float dt)
+  void CVFType::init(float dt,
+                     float process_std_dev,
+                     float measurement_std_dev)
   {
-    transition_matrix = {1, 1, 0, dt};
+    transition_matrix = {1, dt, 0, 1};
     measurement_matrix = {1, 0};
-    process_noise_cov = StateCovType{dt * dt, dt, dt, 1.} * process_std_dev_ * process_std_dev_;
-    measurement_noise_cov = MeasurementCovType{measurement_std_dev_ * measurement_std_dev_};
+    process_noise_cov = StateCovType{dt * dt, dt, dt, 1.} * process_std_dev * process_std_dev;
+    measurement_noise_cov = MeasurementCovType{measurement_std_dev * measurement_std_dev};
   }
 
-  void CVFType::init_separate(float measurement_0,
-                              Separate &separate)
+  void CVFType::init_filter_state(float measurement_0,
+                                  FilterState &fs)
   {
-    separate.state_post = StateType{measurement_0, 0.}; // (phi, delta_phi)
-    separate.error_cov_post = {10., 10., 10., 10.}; // Large uncertainty
+    fs.x_k = StateType{measurement_0, 0.}; // (phi, delta_phi)
+    fs.error_cov_k = {10., 10., 10., 10.}; // Large uncertainty to start with.
   }
+
+// ==============================================================================
+// get_model routine
+// ==============================================================================
 
   static CVFType::StateType get_model(float t)
   {
@@ -260,44 +260,46 @@ namespace camsim
     return CVFType::StateType{deg, deg_per_sec};
   }
 
+// ==============================================================================
+// kal_do_filter_x routine
+// ==============================================================================
 
   void kal_do_filter_x()
   {
-    float dt = 1. / 30.;
-    float process_std_dev = 1; // velocity standard deviation
-    float measurement_std_dev = 1;
-
-    CVFType kf{process_std_dev, measurement_std_dev};
-    CVFType::Separate filt{kf};
-
-    CVFType::StateType process_noise;
-    CVFType::MeasurementType measurement_noise;
-
     Visualizer<500, 500> vis{};
 
-    kf.init(dt);
+    CVFType kf{};
+    CVFType::FilterState fs{};
+
+    CVFType::MeasurementType measurement;
+
+    float dt = 1. / 15.;
+    float process_std_dev = 6; // velocity standard deviation
+    float measurement_std_dev = 2;
+
+    kf.init(dt, process_std_dev, measurement_std_dev);
 
     for (;;) {
       char code = (char) -1;
       float t = 0.0;
 
-      kf.init_separate(get_model(t)(0), filt);
+      kf.init_filter_state(get_model(t)(0), fs);
 
       for (;;) {
         t += dt;
 
-        // Generate the new model state
+        // Generate the new model state as ground truth.
         auto model = get_model(t);
 
-        // Get the predicted state
-        auto prediction = filt.predict();
+        // Get the predicted state.
+        auto prediction = kf.predict(fs);
 
         // Get the measurement as the model with noise.
-        randn(measurement_noise, cv::Scalar::all(0), cv::Scalar::all(measurement_std_dev * measurement_std_dev));
-        auto measurement = kf.measurement_matrix * model + measurement_noise;
+        randn(measurement, cv::Scalar::all(0), cv::Scalar::all(measurement_std_dev * measurement_std_dev));
+        measurement += kf.measurement_matrix * model ;
 
-        // Correct the prediction with the measurement
-        auto update = filt.update(measurement);
+        // Update the prediction with the measurement.
+        auto update = kf.update(fs, measurement);
 
         // Display stuff
         vis.show(model(0), prediction(0), measurement(0), update(0));
