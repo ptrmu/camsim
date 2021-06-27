4fb9d7657c1f77df4121b1c4559580371bde5501
Peter Mullen <ptrmu@outlook.com>
2021-06-26 22:04:50 -0700

    add fixedlag to InterMarkerPoseTest

--- src/smf/inter_marker_pose.cpp
+++ src/smf/inter_marker_pose.cpp
@@ -31,10 +31,27 @@ namespace camsim
     {}
   };
 
+  struct FixedLagData
+  {
+    gtsam::Key t_m0_m1_key_;
+    gtsam::NonlinearFactorGraph graph_;
+    gtsam::Values initial_;
+    gtsam::FixedLagSmoother::KeyTimestampMap timestamps_;
+    gtsam::BatchFixedLagSmoother batch_smoother_;
+
+    FixedLagData(gtsam::Key t_m0_m1_key, double lag,
+                 const gtsam::LevenbergMarquardtParams &params) :
+      t_m0_m1_key_{t_m0_m1_key},
+      graph_{}, initial_{},
+      timestamps_{},
+      batch_smoother_{lag, params}
+    {}
+  };
+
 // ==============================================================================
 // MarkerMarkerStorage class
 // ==============================================================================
-
+#if 0
   template<class Stg>
   class MarkerMarkerStorage
   {
@@ -94,8 +111,9 @@ namespace camsim
     }
   };
 
-  using GraphDataStorage = MarkerMarkerStorage<GraphData>;
-
+  using GraphDataStorage = MarkerMarkerStorage<GraphData>; //
+  using FixedLagDataStorage = MarkerMarkerStorage<FixedLagData>; //
+#endif
 // ==============================================================================
 // InterMarkerPoseTest class
 // ==============================================================================
@@ -114,7 +132,6 @@ namespace camsim
   private:
     Config cfg_;
     fvlam::MarkerModelRunner &runner_;
-    std::map<std::uint64_t, GraphData> graph_data_map_;
 
     const CalInfo::Map k_map_;
     const std::string base_imager_frame_id_;
@@ -123,22 +140,28 @@ namespace camsim
     std::array<gtsam::Point3, 4> corners_f_marker_array_;
     const std::vector<fvlam::Transform3> t_imager0_imagerNs_;
 
-    GraphData &add_or_lookup(std::uint64_t id0, const std::function<GraphData(void)> &make_graph_data)
+    template<class TData>
+    TData &add_or_lookup(std::uint64_t key,
+                         std::map<std::uint64_t, TData> &map,
+                         const std::function<TData(std::uint64_t)> &make_data)
     {
-      auto pair = graph_data_map_.find(id0);
-      if (pair != graph_data_map_.end()) {
+      auto pair = map.find(key);
+      if (pair != map.end()) {
         return pair->second;
       }
 
-      graph_data_map_.emplace(id0, make_graph_data());
-      return graph_data_map_.find(id0)->second;
+      map.emplace(key, make_data(key));
+      return map.find(key)->second;
     }
 
-    GraphData &add_marker_pair_to_graph(
+    template<class TData>
+    TData &add_marker_pair_to_graph(
       const fvlam::MarkerObservations &marker_observations,
       const fvlam::CameraInfo &camera_info,
       const fvlam::Observation &observation0,
-      const fvlam::Observation &observation1)
+      const fvlam::Observation &observation1,
+      std::map<std::uint64_t, TData> &map,
+      const std::function<TData(std::uint64_t)> &make_data)
     {
       auto t_m0_c_key = fvlam::ModelKey::camera(marker_observations.camera_index());
       auto t_m0_m1_key = fvlam::ModelKey::marker_marker(observation0.id(), observation1.id());
@@ -151,12 +174,10 @@ namespace camsim
       auto m0_corners_f_image = observation0.to<std::array<gtsam::Point2, 4>>();
       auto m1_corners_f_image = observation1.to<std::array<gtsam::Point2, 4>>();
 
-      auto &graph_data = add_or_lookup(t_m0_m1_key,
-                                       [t_m0_m1_key]() -> GraphData
-                                       { return GraphData(t_m0_m1_key); });
+      TData &data = add_or_lookup<TData>(t_m0_m1_key, map, make_data);
 
       // Add the factor to the graph
-      graph_data.graph_.emplace_shared<QuadMarker0Marker1Factor>(
+      data.graph_.template emplace_shared<QuadMarker0Marker1Factor>(
         t_m0_c_key, t_m0_m1_key,
         m0_corners_f_image,
         m1_corners_f_image,
@@ -176,14 +197,14 @@ namespace camsim
       auto t_m0_c = t_m0_w * t_w_c;
       auto t_m0_m1 = t_m0_w * t_w_m1;
 
-      if (!graph_data.initial_.exists(t_m0_c_key)) {
-        graph_data.initial_.insert(t_m0_c_key, t_m0_c.to<gtsam::Pose3>().compose(delta));
+      if (!data.initial_.exists(t_m0_c_key)) {
+        data.initial_.insert(t_m0_c_key, t_m0_c.to<gtsam::Pose3>().compose(delta));
       }
-      if (!graph_data.initial_.exists(t_m0_m1_key)) {
-        graph_data.initial_.insert(t_m0_m1_key, t_m0_m1.to<gtsam::Pose3>().compose(delta));
+      if (!data.initial_.exists(t_m0_m1_key)) {
+        data.initial_.insert(t_m0_m1_key, t_m0_m1.to<gtsam::Pose3>().compose(delta));
       }
 
-      return graph_data;
+      return data;
     }
 
     int optimize_and_test(GraphData &graph_data)
@@ -227,15 +248,15 @@ namespace camsim
         RETURN_ONE_IF_FALSE(
           runner_.logger(), "InterMarkerPoseTest optimize_and_test",
           gtsam::assert_equal(t_m0_m1_expected.to<gtsam::Pose3>(),
-            t_m0_m1_actual,
-            runner_.cfg().equals_tolerance_));
+                              t_m0_m1_actual,
+                              runner_.cfg().equals_tolerance_));
       }
       return 0;
     }
 
-    int optimize_and_test_all()
+    int optimize_and_test_all(std::map<std::uint64_t, GraphData> &graph_data_map)
     {
-      for (auto &gdp: graph_data_map_) {
+      for (auto &gdp: graph_data_map) {
         RETURN_IF_NONZERO(
           runner_.logger(), "InterMarkerPoseTest optimize_and_test_all",
           optimize_and_test(gdp.second));
@@ -247,6 +268,7 @@ namespace camsim
     {
       int pair_count = 0;
       int optimizations_count = 0;
+      std::map<std::uint64_t, GraphData> graph_data_map;
 
       for (auto famos = runner_.make_for_all_marker_observations(false);
            famos.test(); famos.next()) {
@@ -257,17 +279,21 @@ namespace camsim
                faop.test(); faop.next()) {
             pair_count += 1;
 
-            add_marker_pair_to_graph(famos.marker_observations(),
-                                     faos.camera_info(),
-                                     faop.observation0(),
-                                     faop.observation1());
+            add_marker_pair_to_graph<GraphData>(
+              famos.marker_observations(),
+              faos.camera_info(),
+              faop.observation0(),
+              faop.observation1(),
+              graph_data_map,
+              [](std::uint64_t key) -> GraphData
+              { return GraphData{key}; });
           }
 
           if (algorithm == 0) {
             optimizations_count += 1;
             RETURN_IF_NONZERO(
               runner_.logger(), "InterMarkerPoseTest observations",
-              optimize_and_test_all());
+              optimize_and_test_all(graph_data_map));
           }
         }
 
@@ -275,7 +301,7 @@ namespace camsim
           optimizations_count += 1;
           RETURN_IF_NONZERO(
             runner_.logger(), "InterMarkerPoseTest observations",
-            optimize_and_test_all());
+            optimize_and_test_all(graph_data_map));
         }
       }
 
@@ -283,7 +309,7 @@ namespace camsim
         optimizations_count += 1;
         RETURN_IF_NONZERO(
           runner_.logger(), "InterMarkerPoseTest observations",
-          optimize_and_test_all());
+          optimize_and_test_all(graph_data_map));
       }
 
       runner_.logger().warn() << "InterMarkerPoseTest optimizations: " << optimizations_count
@@ -291,14 +317,107 @@ namespace camsim
       return 0;
     }
 
-    int fixed_lag_inter_imager_pose()
+    void load_per_camera_inter_imager_factors(const fvlam::MarkerObservations &marker_observations,
+                                              gtsam::NonlinearFactorGraph &graph,
+                                              gtsam::Values &initial,
+                                              gtsam::FixedLagSmoother::KeyTimestampMap *new_timestamps)
     {
-      if (runner_.logger().output_debug()) {
-        gtsam::guardedSetDebug("IncrementalFixedLagSmoother update", true);
-//      assert(gtsam::isDebugVersion());
+      // Create a map of all marker id's that are observed by the base_imager.
+      std::map<std::uint64_t, fvlam::Transform3> observed_ids{};
+      for (auto &observations : marker_observations.observations_synced().v()) {
+        if (observations.imager_frame_id() == base_imager_frame_id_) {
+          for (auto &observation : observations.v()) {
+            for (auto &marker : runner_.model().targets()) {
+              if (marker.id() == observation.id()) {
+                observed_ids.emplace(marker.id(), marker.t_map_marker().tf());
+                break;
+              }
+            }
+          }
+          break;
+        }
+      }
+      if (observed_ids.empty()) {
+        return;
       }
 
-      // Define the smoother lag (in seconds)
+      // For each imager's observations
+      for (auto &observations : marker_observations.observations_synced().v()) {
+
+        // Find the camera_info and K
+        const auto &kp = k_map_.find(observations.imager_frame_id());
+        if (kp == k_map_.end()) {
+          continue;
+        }
+        auto &cal_info = kp->second;
+
+        // For each observation of a marker
+        for (auto &observation : observations.v()) {
+
+          // If this marker is not seen by the base imager, then don't add its factor.
+          // If a marker is only seen by the base imager and not by others, its factor
+          // will get added to the graph. This is OK - inefficient but the optimization
+          // will not fail
+          auto observed_marker = observed_ids.find(observation.id());
+          if (observed_marker == observed_ids.end()) {
+            continue;
+          }
+          auto &t_map_marker = observed_marker->second;
+
+          auto camera_n_key = fvlam::ModelKey::camera_marker(
+            marker_observations.camera_index(), observation.id());
+
+          if (new_timestamps != nullptr) {
+            (*new_timestamps)[camera_n_key] = marker_observations.camera_index();
+          }
+
+          // For each corner of a marker
+          for (std::size_t i = 0; i < observation.corners_f_image().size(); i += 1) {
+
+            if (cal_info.imager_index_ == 0) {
+
+              // imager 0 just does resection to figure its pose
+              graph.emplace_shared<fvlam::ResectioningFactor>(
+                camera_n_key,
+                observation.corners_f_image()[i].to<gtsam::Point2>(),
+                measurement_noise_,
+                corners_f_marker_[i],
+                cal_info.std_cal3ds2_,
+                runner_.logger(), true);
+
+              if (!initial.exists(camera_n_key)) {
+                auto t_marker_camera = t_map_marker.inverse() * marker_observations.t_map_camera();
+                initial.insert(camera_n_key, t_marker_camera.to<gtsam::Pose3>().compose(
+                  gtsam::Pose3(gtsam::Rot3::Rodrigues(-0.1, 0.2, 0.25), gtsam::Point3(0.05, -0.10, 0.20))));
+              }
+
+            } else {
+
+              auto relative_imager_key = fvlam::ModelKey::value(cal_info.imager_index_);
+
+              // imager n uses a factor relative to imager 0
+              graph.emplace_shared<Imager0Imager1Factor>(
+                camera_n_key, relative_imager_key,
+                observation.corners_f_image()[i].to<gtsam::Point2>(),
+                measurement_noise_,
+                corners_f_marker_[i],
+                cal_info.std_cal3ds2_,
+                runner_.logger(), true);
+
+              if (!initial.exists(relative_imager_key) && new_timestamps == nullptr) {
+                initial.insert(relative_imager_key, gtsam::Pose3{});
+              }
+            }
+          }
+        }
+      }
+    }
+
+    int fixed_lag()
+    {
+      std::map<std::uint64_t, FixedLagData> fixed_lag_data_map;
+
+      // Define the smoother lag in camera indices
       double lag = 3.0;
 
       // Create a fixed lag smoother
@@ -311,23 +430,42 @@ namespace camsim
       params.setMaxIterations(2048);
       gtsam::BatchFixedLagSmoother smootherBatch(lag, params);
 
-      // The Incremental version uses iSAM2 to perform the nonlinear optimization
-      gtsam::ISAM2Params parameters;
-      parameters.relinearizeThreshold = 0.0; // Set the relin threshold to zero such that the batch estimate is recovered
-      parameters.relinearizeSkip = 1; // Relinearize every time
-      gtsam::IncrementalFixedLagSmoother smootherISAM2(lag, parameters);
-
       // Create containers to store the factors and linearization points that
       // will be sent to the smoothers
       gtsam::NonlinearFactorGraph new_factors;
       gtsam::Values new_values;
       gtsam::FixedLagSmoother::KeyTimestampMap new_timestamps;
 
-      // Add the relative imager pose initial values. Set the time as greater than the number of camera positions.
+      // Add the relative imager pose initial values.
       for (std::size_t i = 1; i < t_imager0_imagerNs_.size(); i += 1) {
         auto t_i0_iN_key = fvlam::ModelKey::value(i);
         new_values.insert(t_i0_iN_key, t_imager0_imagerNs_[i].to<gtsam::Pose3>());
 //        new_timestamps[t_i0_iN_key] = runner_.marker_observations_list_perturbed().size();
+      }
+
+
+      for (auto famos = runner_.make_for_all_marker_observations(false);
+           famos.test(); famos.next()) {
+
+
+        for (auto faos = fvlam::MarkerModelRunner::ForAllObservations(famos.marker_observations(),
+                                                                      runner_.model().camera_info_map());
+             faos.test(); faos.next()) {
+          for (auto faop = fvlam::MarkerModelRunner::ForAllObservationPair(faos.observations());
+               faop.test(); faop.next()) {
+
+            add_marker_pair_to_graph<FixedLagData>(
+              famos.marker_observations(),
+              faos.camera_info(),
+              faop.observation0(),
+              faop.observation1(),
+              fixed_lag_data_map,
+              [](std::uint64_t key) -> FixedLagData
+              { return FixedLagData{key, 3.0, gtsam::LevenbergMarquardtParams{}}; });
+          }
+        }
+
+
       }
 
       for (std::size_t i_camera = 0; i_camera < runner_.marker_observations_list_perturbed().size(); i_camera += 1) {
@@ -347,9 +485,6 @@ namespace camsim
           }
 
           smootherBatch.update(new_factors, new_values, new_timestamps);
-//          smootherISAM2.update(new_factors, new_values, new_timestamps);
-//          smootherISAM2.update();
-//          smootherISAM2.update();
 
           gttoc(fixed_lag_inter_imager_pose_loop);
 
@@ -392,7 +527,7 @@ namespace camsim
 
   public:
     InterMarkerPoseTest(Config cfg, fvlam::MarkerModelRunner &runner) :
-      cfg_{cfg}, runner_{runner}, graph_data_map_{},
+      cfg_{cfg}, runner_{runner},
       k_map_{CalInfo::MakeMap(runner_.model().camera_info_map())},
       base_imager_frame_id_{CalInfo::make_base_imager_frame_id(k_map_)},
       measurement_noise_{gtsam::noiseModel::Isotropic::Sigma(16, 1.0)},
@@ -405,8 +540,6 @@ namespace camsim
 
     int operator()()
     {
-      graph_data_map_.clear();
-
       switch (cfg_.algorithm_) {
         default:
         case 0:
@@ -415,7 +548,7 @@ namespace camsim
           return all_camera(cfg_.algorithm_);
 
         case 3:
-          return fixed_lag_inter_imager_pose();
+          return fixed_lag();
       }
     }
   };
